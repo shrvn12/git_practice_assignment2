5f5aa88e12ecd2bcf38f81e5c5996206b8293fa1
shrvn12 <singhshravan1208@gmail.com>
2022-09-30 18:38:21 +0530

    completed the code

--- check_prime.js
+++ check_prime.js
@@ -1,7 +1,13 @@
 let num = 5;
 let factor = 0;
-for (let x = 1; x < num; x++) {
+for (let x = 1; x < num**0.5; x++) {
     if(num % 2 === 0) {
       factor++
     }
 }
+if (factor == 1) {
+   console.log("yes");
+   }
+else {
+     console.log("no")
+     }
